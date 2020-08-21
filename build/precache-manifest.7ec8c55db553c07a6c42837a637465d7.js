55d22e2d6f472cb60cce65683286e98dec72e996
DextrousDavid <aimakhededavido@gmail.com>
2020-08-21 09:15:18 +0100

    added netlify.noml file to build folder and enabled build

---
+++ build/precache-manifest.7ec8c55db553c07a6c42837a637465d7.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "7176a7d58bd8f7d7cd6f",
+    "url": "https://github.com/DextrousDavid/static/js/0.26b81d15.chunk.js"
+  },
+  {
+    "revision": "82f72281b3deaf87d79d",
+    "url": "https://github.com/DextrousDavid/static/css/main.a33314ad.chunk.css"
+  },
+  {
+    "revision": "82f72281b3deaf87d79d",
+    "url": "https://github.com/DextrousDavid/static/js/main.df4a6785.chunk.js"
+  },
+  {
+    "revision": "ec08bfc8bd9e851f1182",
+    "url": "https://github.com/DextrousDavid/static/js/runtime~main.5a84b207.js"
+  },
+  {
+    "revision": "7bb65d9e1d138588a015",
+    "url": "https://github.com/DextrousDavid/static/js/3.5dae6c9e.chunk.js"
+  },
+  {
+    "revision": "91a64bf2b61bd71e3ec0",
+    "url": "https://github.com/DextrousDavid/static/js/4.ecdd1220.chunk.js"
+  },
+  {
+    "revision": "c10683a3ac899e732a3e",
+    "url": "https://github.com/DextrousDavid/static/js/5.ed246752.chunk.js"
+  },
+  {
+    "revision": "1970e2c0cb5fa8a4067c",
+    "url": "https://github.com/DextrousDavid/static/js/6.cd6d08ad.chunk.js"
+  },
+  {
+    "revision": "c9d353a0aff16c06991a694ad3e04b24",
+    "url": "https://github.com/DextrousDavid/static/media/sidebar-4.c9d353a0.jpg"
+  },
+  {
+    "revision": "2f1ab09742f44e31b19a7f2df39629ea",
+    "url": "https://github.com/DextrousDavid/static/media/Ojie.2f1ab097.jpg"
+  },
+  {
+    "revision": "dec58cb4e10d1c06f3daa5216dd24f1f",
+    "url": "https://github.com/DextrousDavid/static/media/aliko-dangote.dec58cb4.jpg"
+  },
+  {
+    "revision": "df0a4d8bf3e93b2db511ce40b82e60e6",
+    "url": "https://github.com/DextrousDavid/static/media/amaka.df0a4d8b.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "https://github.com/DextrousDavid/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://github.com/DextrousDavid/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "a9a0ec6482226ea8098fc71a3fababe0",
+    "url": "https://github.com/DextrousDavid/static/media/product_640-1.a9a0ec64.jpg"
+  },
+  {
+    "revision": "da0b0457353c6555a5ba4535597f2187",
+    "url": "https://github.com/DextrousDavid/static/media/product_640-2.da0b0457.jpg"
+  },
+  {
+    "revision": "64ed27c9aca0306b4185b82e144cdedd",
+    "url": "https://github.com/DextrousDavid/static/media/product_640-3.64ed27c9.jpg"
+  },
+  {
+    "revision": "2110d8cd7d70f6792b4c2d517cb7f593",
+    "url": "https://github.com/DextrousDavid/static/media/product_640-4.2110d8cd.jpg"
+  },
+  {
+    "revision": "bf8840bd88a80c14418b238a7f1beb24",
+    "url": "https://github.com/DextrousDavid/static/media/product_640-5.bf8840bd.jpg"
+  },
+  {
+    "revision": "f9249755162af20121af258ca1ffc578",
+    "url": "https://github.com/DextrousDavid/static/media/product_640-6.f9249755.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "https://github.com/DextrousDavid/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "https://github.com/DextrousDavid/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "https://github.com/DextrousDavid/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "de88253696b81598494dd6bd5c0b087e",
+    "url": "https://github.com/DextrousDavid/static/media/100_6.de882536.jpg"
+  },
+  {
+    "revision": "1dce100ffdb944226acf9db666a57319",
+    "url": "https://github.com/DextrousDavid/static/media/100_7.1dce100f.jpg"
+  },
+  {
+    "revision": "a4be0bbf3a08f9de24548aa6765af93f",
+    "url": "https://github.com/DextrousDavid/static/media/100_8.a4be0bbf.jpg"
+  },
+  {
+    "revision": "798c02eb53d02e11c3d8a6588cdf5033",
+    "url": "https://github.com/DextrousDavid/static/media/100_9.798c02eb.jpg"
+  },
+  {
+    "revision": "56d054ffdf15ac516d7ba19fa9a33ad5",
+    "url": "https://github.com/DextrousDavid/static/media/100_10.56d054ff.jpg"
+  },
+  {
+    "revision": "336faadc989ea15bbe2c0fb30e20bcd7",
+    "url": "https://github.com/DextrousDavid/static/media/100_11.336faadc.jpg"
+  },
+  {
+    "revision": "1e0889afa5d1fa653fef4f8b5d3272d2",
+    "url": "https://github.com/DextrousDavid/static/media/100_12.1e0889af.jpg"
+  },
+  {
+    "revision": "63265e929c9f7442ea1853d2dc19a188",
+    "url": "https://github.com/DextrousDavid/static/media/100_13.63265e92.jpg"
+  },
+  {
+    "revision": "595b8cbdd2b7f27cf7d38b895a9b56c9",
+    "url": "https://github.com/DextrousDavid/static/media/100_14.595b8cbd.jpg"
+  },
+  {
+    "revision": "50e53aa928cb26fc8a347a5e675e72dc",
+    "url": "https://github.com/DextrousDavid/static/media/samuel-aimakhede.50e53aa9.jpg"
+  },
+  {
+    "revision": "011de202a05cb15e2ea3ad31b336f99a",
+    "url": "https://github.com/DextrousDavid/static/media/mike-adenuga.011de202.jpg"
+  },
+  {
+    "revision": "3df8870c57bc47e26acda81756a253ea",
+    "url": "https://github.com/DextrousDavid/static/media/james.3df8870c.jpg"
+  },
+  {
+    "revision": "e81bf89bddb27dae94d93e77a186d494",
+    "url": "https://github.com/DextrousDavid/static/media/sandra-jonathan.e81bf89b.jpg"
+  },
+  {
+    "revision": "a8464f196d608572bf2a2d16b91190e1",
+    "url": "https://github.com/DextrousDavid/static/media/richard-m-damijo.a8464f19.jpg"
+  },
+  {
+    "revision": "1029785b2f69ee9daee4847dfd15b5f7",
+    "url": "https://github.com/DextrousDavid/index.html"
+  }
+];
